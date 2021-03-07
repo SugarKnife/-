5437fe04156055074e90557bfe3764d748293b9b
jiangjun <171826342@qq.com>
2021-03-07 18:54:29 +0800

    完成了个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,43 @@
+$(function(){
+    let form=layui.form
+    let layer=layui.layer
+    form.verify({
+        nickname:function(value){
+            if(value.length>6){
+                return "昵称长度必须在 1 ~ 6 位之间"
+            }
+        }
+    })
+    initUserInfo()
+    function initUserInfo(){
+        $.ajax({
+            type:"get",
+            url:"/my/userinfo",
+            success:function(res){
+                if(res.status!==0){
+                    return layer.msg("获取用户信息失败")
+                }
+                form.val("formUserInfo",res.data)
+            }
+        })
+    }
+    $("#btnRest").on("click",function(e){
+        e.preventDefault()
+        initUserInfo()
+    })
+    $(".layui-form").on("submit",function(e){
+        e.preventDefault()
+        $.ajax({
+            type:"post",
+            url:"/my/userinfo",
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status!==0){
+                    return layer.msg("更新信息失败")
+                }
+                layer.msg("更新信息成功")
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
